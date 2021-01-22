cfd822e85f4b7447dda94ef8db1b7c12e2f4491c
wanting <kashjack@163.com>
2021-01-22 17:15:40 +0800

    no message

--- shiyanlou001.py
+++ shiyanlou001.py
@@ -1,16 +1,14 @@
 # !/usr/bin/python
 # -*- coding: UTF-8 -*-
-import numpy as np
-import pandas as pd
+import numpy
+import pandas
 
-df1 = pd.DataFrame({'A': np.random.randn(10), 'B': np.random.randn(10)})
-df2 = pd.DataFrame({'C': np.random.randn(10), 'D': np.random.randn(10)})
+df1 = pandas.DataFrame({'A': numpy.random.randn(10), 'B': numpy.random.randn(10)})
+df2 = pandas.DataFrame({'C': numpy.random.randn(10), 'D': numpy.random.randn(10)})
 
 # 写入数据文件
-# df.to_csv('test.csv', encoding='utf-8', index=False)
-# df.to_excel('test.xlsx', encoding='utf-8', index=False)
+df1.to_csv('test.csv', encoding='utf-8', index=False)
+df2.to_excel('test.xlsx', encoding='utf-8', index=False)
 
-df1.to_hdf('test.h5', key='df1')
-df2.to_hdf('test.h5', key='df2', format='table')
 
 print("*****示例文件写入成功*****")
